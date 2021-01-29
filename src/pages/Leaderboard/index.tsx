4ccd54382e871e1f85a4c08220f0fd28bc9dbd25
Roland Killian <roland.t.killian@gmail.com>
2021-01-29 14:28:35 -0500

    Added leader board page

---
+++ src/pages/Leaderboard/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Leaderboard from './Leaderboard';
+
+function UserPage() {
+  return (
+    <Container maxWidth="lg">
+      <Box my={4}>
+        <Leaderboard />
+      </Box>
+    </Container>
+  );
+}
+
+export default UserPage;
