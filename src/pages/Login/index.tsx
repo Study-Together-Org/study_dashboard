e61d69dc513fb0ce35e0cf3bc550aa66a381b520
Cole Killian <g.cole.killian@gmail.com>
2021-07-19 20:08:53 -0700

    fix user login redirect

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Box from '@material-ui/core/Box'
 import Card from '@material-ui/core/Card'
@@ -9,8 +9,23 @@ import Typography from '@material-ui/core/Typography'
 import { api as axios } from '../../services'
 import { Redirect } from 'react-router-dom'
 
+const delay = 1
+
 function UserPage() {
-  window.location.replace(`${axios.defaults.baseURL}/login`)
+  useEffect(() => {
+    let timer1 = setTimeout(
+      () => window.location.replace(`${axios.defaults.baseURL}/login`),
+      delay * 1500
+    )
+
+    // this will clear Timeout
+    // when component unmount like in willComponentUnmount
+    // and show will not change to true
+    return () => {
+      clearTimeout(timer1)
+    }
+  }, [])
+
   return <Typography>Redirecting to login...</Typography>
 }
 
